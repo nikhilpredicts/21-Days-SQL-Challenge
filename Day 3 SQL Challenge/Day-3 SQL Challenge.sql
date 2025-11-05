9c1861b2493f1ba9e95d852c15737e9321617b82
Nikhil Karaka <karakanikhil2003@gmail.com>
2025-11-05 21:05:08 +0530

    Add files via upload

---
+++ Day 3 SQL Challenge/Day-3 SQL Challenge.sql
@@ -0,0 +1,17 @@
+use hospital;
+
+-- Day-3 Challenge
+/* Retrieve the top 5 weeks with the highest patient refusals across all services,
+ showing week, service, patients_refused, and patients_request. Sort by patients_refused in descending order. */
+SELECT week,service,patients_refused,patients_request 
+FROM services_weekly ORDER BY patients_refused DESC LIMIT 5;
+
+-- Practice Questions:
+-- List all patients sorted by age in descending order.
+SELECT * FROM patients ORDER BY age desc;
+
+-- Show all services_weekly data sorted by week number ascending and patients_request descending.
+SELECT * FROM services_weekly ORDER BY week ASC,patients_request desc;
+
+-- Display staff members sorted alphabetically by their names.
+SELECT * FROM staff ORDER BY staff_name ASC;
